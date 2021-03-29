a7c07fa716bb0b70cc329c388ca7405b855c906a
josfemova <josfemova@gmail.com>
2021-03-28 22:17:01 -0600

    add some ui messages

--- src/wazelog.pl
+++ src/wazelog.pl
@@ -6,25 +6,22 @@
 %		Alejandro Soto Chacon	2019008164
 %Semestre I 2021
 
-
-
-
+%tipo_sitio(clasificacion).
 tipo_sitio(supermercado).
 tipo_sitio(parque).
 tipo_sitio(mall).
 tipo_sitio(tienda).
 tipo_sitio(ferreteria).
-
-%sitio(tipo,nombre);
-
+%ciudad('lugar').
 ciudad('san jose').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
-ciudad('').
+ciudad('cartago').
+ciudad('san pedro').
+ciudad('tres rios').
+
+
+saludo:-writeln("Bienvenido a WazeLog, la mejor logica de llegar a su destino"),
+	writeln("Por favor indiqueme donde se encuentra").
+despedida:-writeln("Muchas gracias por utilizar Wazelog!").
+preg_intermedio(1):-write("Algun destino intermedio?"),!.
+preg_intermedio(_):-write("Algun otro destino intermedio?").
+preg_direccion(Lugar):-format("Donde se encuentra ~w?", [Lugar]).
