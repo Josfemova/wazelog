d844fb7674a7bf6f93227c1865fbde2a07709325
josfemova <josfemova@gmail.com>
2021-03-28 20:24:38 -0600

    add base doc header to pl file

--- src/wazelog.pl
+++ src/wazelog.pl
@@ -1,3 +1,14 @@
+%Instituto Tecnologico de Costa Rica
+%Area Academica de Ingenieria en Computadores
+%Lenguajes, Compialdores e Interpretes
+%Estudiantes
+%		Jose Morales Vargas		2019024270
+%		Alejandro Soto Chacon	2019008164
+%Semestre I 2021
+
+
+
+
 tipo_sitio(supermercado).
 tipo_sitio(parque).
 tipo_sitio(mall).
